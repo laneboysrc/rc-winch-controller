260ba29b307e2e8c2d7243078e2a0dee2c763f0f
LANE Boys RC <laneboysrc@gmail.com>
2013-07-04 22:17:22 +0800

    Start implementation of the winch controller, uart reader

--- firmware/uart-input.c
+++ firmware/uart-input.c
@@ -1,6 +1,31 @@
 #include <pic12f1840.h>
 
+extern unsigned char winch_mode;
+
+#define FOSC 16                 // Osc frequency in MHz
+#define BAUDRATE 38400          // Desired baudrate
+#define BRGH_VALUE 1            // Either 0 or 1
+#define SPBRG_VALUE (((10*FOSC*1000000/((64-(48*BRGH_VALUE))*BAUDRATE))+5)/10)-1
+
+
 void Init_input(void) {
-    return;
+	TRISA1 = 1;     // RX port must be 1 for UART to work
+	TRISA2 = 1;     // TX port must be 1 for UART to work
+
+	SPBRG = SPBRG_VALUE;	// Baud Rate register, calculated by macro
+	BRGH = BRGH_VALUE;
+
+	SYNC=0;			// Disable Synchronous/Enable Asynchronous
+	SPEN=1;			// Enable serial port
+	TXEN=0;			// Disable transmission mode
+	CREN=1;			// Enable reception mode
+}
+
+void Read_input(void) {
+	while (!RCIF) {
+	    ;	            // Wait until data recieved
+    }
+	    
+    winch_mode = RCREG;	
 }
 
--- firmware/winch-controller.c
+++ firmware/winch-controller.c
@@ -1,11 +1,34 @@
 #include <pic12f1840.h>
 
-char test;
-
 extern void Init_input(void);
+extern void Read_input(void);
+
+unsigned char winch_mode = 0;
+
+void Init_hardware(void) {
+    //-----------------------------
+    // Clock initialization
+    OSCCON = 0b01111010;    // 4x PLL disabled, 16 MHz HF, Internal oscillator
+
+    //-----------------------------
+    // IO Port initialization
+    PORTA = 0;
+    LATA = 0;
+    ANSELA = 0;
+    TRISA = 0b11111111;     // Make all ports A input
+    APFCON = 0b00000000;    // Use RA0/RA1 for UART TX/RX
+    
+    //-----------------------------
+    // Initialize Timer1 for 1 MHz operation
+    T1CON = 0b00100000; 
+}
 
 void main(void) {
-    test=0;
+    Init_hardware();
     Init_input();
+    
+    while (1) {
+        Read_input();
+    }
 }
 
