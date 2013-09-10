8a6bc5e7368720f11b8435fc75a622e2bd27293d
Werner Lane <laneboysrc@gmail.com>
2013-09-10 10:01:22 +0800

    Add documentation

--- firmware/uart-input.c
+++ firmware/uart-input.c
@@ -8,6 +8,11 @@ extern unsigned char winch_mode;
 #define SPBRG_VALUE (((10*FOSC*1000000/((64-(48*BRGH_VALUE))*BAUDRATE))+5)/10)-1
 
 
+/*****************************************************************************
+ Init_input()
+
+ Called once during start-up of the firmware. 
+ ****************************************************************************/
 void Init_input(void) {
 	SPBRG = SPBRG_VALUE;	// Baud Rate register, calculated by macro
 	BRGH = BRGH_VALUE;
@@ -18,7 +23,19 @@ void Init_input(void) {
 	CREN=1;			// Enable reception mode
 }
 
+
+/*****************************************************************************
+ Read_input
+
+ Called once per mainloop to read the input signal.
+ This function shall set the global variable winch_mode depending on the  
+ state transition logic desired.
+ ****************************************************************************/
 void Read_input(void) {
+    // Note that for the UART input the state transition logic is handled
+    // in whoever is sending the UART commands, the winch controller is 
+    // just setting winch_mode to whatever value comes in.
+
 	do {
 	    // Overflow error?
 	    // Yes: toggle CREN and flush the receive register to clear the overflow
